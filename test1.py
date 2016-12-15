377a675646ea283b39e6c164fcb4849f91740106
lvanhulle <lvanhulle@wisc.edu>
2016-12-15 17:57:35 -0600

    Trying to see if I can inspect the local variables of a function

--- test1.py
+++ test1.py
@@ -5,4 +5,17 @@ Created on Thu Dec 15 16:00:46 2016
 @author: lvanhulle
 """
 
+def getLocals(func):
+    loc = None
+    def wrapped(*args, **kwargs):
+        nonlocal loc
+        loc = locals()
+        print(locals())
+        return func(*args, **kwargs)
+    print(loc)
+    return wrapped
 
+def h(a=1, b=2):
+    i = 3
+    j = 4
+    return a+b+i+j
