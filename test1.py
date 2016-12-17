b525732a11ede880668483db6bda7a727ea48ec1
VanHulleOne <lvanhulle@wisc.edu>
2016-12-17 13:51:52 -0600

    Slowly making progress

--- test1.py
+++ test1.py
@@ -4,18 +4,27 @@ Created on Thu Dec 15 16:00:46 2016
 
 @author: lvanhulle
 """
-g = 10
 
-def getLocals(func):
-    loc = None
-    def wrapped(*args, **kwargs):
-        print(locals(), "Yo")
-        return func(*args, **kwargs)
-    print(loc)
-    return wrapped
 
-def h(a=1, b=2):
-    i = 3
-    j = 4
-    print(locals())
-    return a+b+i+j
+
+import level
+
+@level.boundry
+def b1():
+    points = ((0,0), (2,0), (2,3))
+    return points
+
+level.parameters(pathWidth = 0.5,
+               layerHeight = 0.2,
+               solidityRatio = 0.98,
+               shiftX = 50,
+               shiftY = 20,
+               numShells = 2,
+              )
+
+
+             
+#@level.region(numShells = 1,
+#              solidityRatio = 0.76)
+#def region1(b1):
+#    pass
