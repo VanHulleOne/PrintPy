1f93709a5f061fb277ebac403552fe10657c7081
lvanhulle <lvanhulle@wisc.edu>
2016-12-21 14:53:47 -0600

    Prettier by the minute

--- test2.py
+++ test2.py
@@ -19,30 +19,30 @@ def reset():
     
 class SpaceCurve:
     
+    rules = NotImplemented
+    consts = NotImplemented
+    axiom = NotImplemented
+    
     def __init__(self, sideLength=7):
         self.sideLength = sideLength
         self.color = cycle(colors)
         t.color(next(self.color))
         
-    def curve_gen(self, level, seq=None):
+    def curve_gen(self, level, seq):
         cls = type(self)
-        
-        if seq is None:
-            seq = cls.a_seq
-        
+
         if level == 1:
             for i in seq:
-                yield i
+                if i in cls.consts:
+                    yield cls.consts[i]
             return None
         if level == 2:
             t.color(next(self.color))
         for i in seq:
-            if i is cls.a:
-                yield from self.curve_gen(level-1)
-            elif i is cls.b:
-                yield from self.curve_gen(level-1, cls.b_seq)
-            else:
-                yield i
+            if i in cls.rules:
+                yield from self.curve_gen(level-1, cls.rules[i])
+            elif i in cls.consts:
+                yield cls.consts[i]
                 
     def __call__(self, level):
         cls = type(self)
@@ -50,35 +50,40 @@ class SpaceCurve:
             i(self.sideLength)
     
 class Gosper(SpaceCurve):
-    a = lambda sideLength : t.forward(sideLength)
-    p = lambda _ : t.left(60)
-    m = lambda _ : t.left(-60)
-    b = lambda sideLength : t.forward(sideLength)   
-    
-    a_seq = (a,m,b,m,m,b,p,a,p,p,a,a,p,b,m)
-    b_seq = (p,a,m,b,b,m,m,b,m,a,p,p,a,p,b)
-    axiom = (a,)
+    rules = {'a' : 'a-b--b+a++aa+b-',
+            'b' : '+a-bb--b-a++a+b',
+            }
+            
+    consts = {'a' : lambda sideLength : t.forward(sideLength),
+              '+' : lambda _ : t.left(60),
+              '-' : lambda _ : t.left(-60),
+              'b' : lambda sideLength : t.forward(sideLength),
+              }
+
+    axiom = 'a'
     
 
 class Hilbert(SpaceCurve):
-    a = lambda _ : None
-    b = lambda _ : None
-    f = lambda sideLength : t.forward(sideLength)
-    p = lambda _ : t.right(90)
-    m = lambda _ : t.right(-90)
+    consts = {'f' : lambda sideLength : t.forward(sideLength),
+              '+' : lambda _ : t.right(90),
+              '-' : lambda _ : t.right(-90),
+              }
     
-    a_seq = (m,b,f,p,a,f,a,p,f,b,m)
-    b_seq = (p,a,f,m,b,f,b,m,f,a,p)
-    axiom = (a,)
+    rules = {'a' : '-bf+afa+fb-',
+             'b' : '+af-bfb-fa+',
+             }
+
+    axiom = 'a'
 
 class Moore(SpaceCurve):
-    a = lambda _ : None
-    b = lambda _ : None
-    f = lambda sideLength : t.forward(sideLength)
-    p = lambda _ : t.left(90)
-    m = lambda _ : t.left(-90)
-    
-    a_seq = (m,b,f,p,a,f,a,p,f,b,m)
-    b_seq = (p,a,f,m,b,f,b,m,f,a,p)
-    axiom = (a,f,a,p,f,p,a,f,a)
+    consts = {'f' : lambda sideLength : t.forward(sideLength),
+              '+' : lambda _ : t.left(90),
+              '-' : lambda _ : t.left(-90),
+              }
+              
+    rules = {'l' : '-rf+lfl+fr-',
+             'r' : '+lf-rfr-fl+',
+             }
+
+    axiom = 'lfl+f+lfl'
                   
